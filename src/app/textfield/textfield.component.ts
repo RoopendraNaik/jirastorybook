450ba042e4512c824faa000bbe1c299983268de4
vijyetas <55085066+vijyetas@users.noreply.github.com>
2020-07-29 14:28:18 +0530

    Update textfield.component.ts

--- src/app/textfield/textfield.component.ts
+++ src/app/textfield/textfield.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, ViewChild } from '@angular/core';
 import { FormGroup, FormBuilder, Validators, FormControl } from '@angular/forms';
+import { MatInput } from '@angular/material/input';
 
 @Component({
   selector: 'app-textfield',
@@ -7,21 +8,23 @@ import { FormGroup, FormBuilder, Validators, FormControl } from '@angular/forms'
   styleUrls: ['./textfield.component.scss']
 })
 export class TextfieldComponent implements OnInit {
-
-  @Input() placeholder= "e.g placeholder";
-  @Input() label= "label";
+  @Input() placeholder= "";
+  @Input() label= "Label";
   @Input() type = "text";
   @Input() FormControlName: FormControl;
   @Input() errorMessage= "This field is required";
   @Input() hint: string;
+  @Input() floatLabelValue = "never";
+  @Input() inputText = "";
+  @Input() isDisabled = false;
+  public form: FormGroup;
 
   dummyForm: FormGroup;
   constructor(private fb: FormBuilder) {
-    console.log("logging passed values");
     console.log(this.FormControlName);
+
   }
 
   ngOnInit(): void {
-  }
-
-}
+    console.log('---inisde ngonit');
+}}
