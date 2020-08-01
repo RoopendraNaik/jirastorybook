b141ef72089586090ee09436154d59ffdc3b6a19
vijyetas <55085066+vijyetas@users.noreply.github.com>
2020-08-01 17:59:58 +0530

    Update ui-button.stories.ts

--- src/app/ui-button/ui-button.stories.ts
+++ src/app/ui-button/ui-button.stories.ts
@@ -1,6 +1,6 @@
 import { UiButtonComponent } from "./ui-button.component";
 import { storiesOf, moduleMetadata } from '@storybook/angular';
-import { boolean, number, text, withKnobs } from '@storybook/addon-knobs';
+import { boolean, number, text, withKnobs, button,color  } from '@storybook/addon-knobs';
 import { AttributesStatesDefault, withPseudo } from "@ergosign/storybook-addon-pseudo-states-angular";
 // export default {
 //     title : 'Button',
@@ -19,40 +19,63 @@ import { AttributesStatesDefault, withPseudo } from "@ergosign/storybook-addon-p
 //         text: 'Button'
 //     } 
 // });
-   
+
 //   export const withKnobs = () => ({
 //     component: Button,
 //     props: {
 //       text: text('text', 'Hello Storybook'), // The first param of the knob function has to be exactly the same as the component input.
 //     },
 //   });
-
+var props;
 storiesOf('Button', module)
-.addDecorator(withKnobs)
+    .addDecorator(withKnobs)
     .addDecorator(
         moduleMetadata({
             declarations: [UiButtonComponent],
             entryComponents: [UiButtonComponent],
         })
     )
-    .add('default', () => {
+    // .add('With Knobs', () => {
+
+    //     return {
+    //         template: `<app-ui-button [text]="text" [bgcolor]="color" state="default"></app-ui-button>`,
+    //         props: {
+    //             text: text('Button text', 'Hello Button'),
+    //             color : color('Button Background Colour', 'white')
+    //           }
+           
+    //     }
+    // }
+    // )
+    .add('Default', () => {
         return {
             template: `<app-ui-button text="Button" state="default"></app-ui-button>`,
         }
-    }
-    )
-    .add('hover', () => {
+    })
+    .add('Hover', () => {
         return {
             template: `<app-ui-button text="Button" state="hover"></app-ui-button>`,
-        }    
+        }
     })
-    .add('focus', () => {
+    .add('Focus', () => {
         return {
             template: `<app-ui-button text="Button" state="focus"></app-ui-button>`,
-        }    
+        }
     })
-    .add('disabled', () => {
+    .add('Disabled', () => {
         return {
             template: `<app-ui-button text="Button" state="disabled"></app-ui-button>`,
         }
     })
+    .add('Knobs', () => {
+
+        return {
+            template: `<app-ui-button [text]="text" [bgcolor]="color" state="default"></app-ui-button>`,
+            props: {                
+                color : color('Button Background Colour', 'white'),
+                text: text('Button text', 'Button Text'),
+              }
+           
+        }
+    }
+    )
